4e2f5226dbde66d1c659d035d2ecbb29c88c5871
ADMIN-PC\ADMIN <jaque.nevees2@gmail.com>
2019-02-27 22:29:09 -0300

    versão inicial do projeto

---
+++ src/main/java/br/edu/iftm/extensao/SistemaExtensaoSts1Application.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.extensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExtensaoSts1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExtensaoSts1Application.class, args);
+	}
+
+}
